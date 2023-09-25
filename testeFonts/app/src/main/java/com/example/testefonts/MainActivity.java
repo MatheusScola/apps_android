2251b22c07f945ec88d87d626b70057da60c9217
MatheusScola <matheus.fernan01@gmail.com>
2023-09-24 23:27:08 -0300

    Modificando interface para temas claros e escuros

--- testeFonts/app/src/main/java/com/example/testefonts/MainActivity.java
+++ testeFonts/app/src/main/java/com/example/testefonts/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.testefonts;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
